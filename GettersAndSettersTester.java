8985706a4787026afd0ff54426be5ca0e99a57f5
JyothchandraG <50104274+JyothchandraG@users.noreply.github.com>
2023-08-29 11:18:56 -0400

    Add files via upload

---
+++ GettersAndSettersTester.java
@@ -0,0 +1,11 @@
+public class GettersAndSettersTester {
+    public static void main(String[] args) {
+        GettersAndSetters g=new GettersAndSetters();
+        g.setStdid(101);
+        g.setStdname("Ankit");
+        g.setStdaddress("Guntur 522001");
+
+        // System.out.println(g.getStdid() + " " + g.getStdname() + " " +
+        System.out.println(g);
+    }
+}
